a26025679c83c5149beaecf3e12f33a05f08d86e
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-07-01 07:31:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "21827814"
+API_HASH = "af097d21fa5049b0ed0d614ed3b885cd"
+
+BOT_TOKEN = "7369149161:AAFAoyZ818kS-St-mpcy2TTumzFlLFZWAX8"
+#OWNER_ID = "7376728791"
+OWNER_ID = "7376728791"
+MONGO_DB_URI = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
+MUST_JOIN = "nexz_support"
